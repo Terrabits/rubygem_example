3d9036cb9c84dd76ecd6d0146a647260b78eb542
Nick Lalic <nick.lalic@gmail.com>
2016-07-13 00:56:10 -0700

    More stuff

--- lib/hola_nbl/my_class.rb
+++ lib/hola_nbl/my_class.rb
@@ -1,8 +1,12 @@
+# Will not load unless explicitly loaded with:
+# require 'hola_nbl/my_class'
 
 module HolaNbl
   class MyClass
+
     def motivational_speech
       puts "Don't get married Morty. Break the cycle. Rise above it. Focus on science."
     end
+    
   end
 end
